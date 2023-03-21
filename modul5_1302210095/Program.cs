755dba0bba46471058ec2c0cb80f03858a3fb9a0
PC14-IFLAB <rahma.sakti24@gmail.com>
2023-03-21 16:32:20 +0700

    menambahkan class SimpleDataBase

--- modul5_1302210095/Program.cs
+++ modul5_1302210095/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        SimpleDataBase<double> data = new SimpleDataBase<double>();
+
+        for(int inputSize = 3; inputSize > 0; inputSize--)
+        {
+            double input = Convert.ToDouble(Console.ReadLine());
+            data.AddNewData(input);
+        }
+        data.PrintAllData();
+    }
+}
+
+class SimpleDataBase<T>
+{
+    private List<T> storeData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        this.storeData = new List<T> { };
+        this.inputDates = new List<DateTime> { };
+    }
+    public void AddNewData(T data)
+    {
+        this.storeData.Add(data);
+        this.inputDates.Add(DateTime.Now);
+    }
+    public void PrintAllData()
+    {
+        for (int index = 0; index < storeData.Count; index++)
+        {
+            Console.WriteLine("Data 1 berisi: " + storeData[index] + ", yang disimpan pada waktu " + inputDates[index]);
+        }
+    }
+}
