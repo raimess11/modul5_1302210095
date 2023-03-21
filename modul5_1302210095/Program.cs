4e580299174d9ae91ceab770d84ef24e94be6192
PC14-IFLAB <rahma.sakti24@gmail.com>
2023-03-21 16:03:08 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- modul5_1302210095/Program.cs
+++ modul5_1302210095/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, generic!");
+        Console.WriteLine(Penjumlahan.JumlahTigaAngka<double>(13d, 02d, 10d));
+    }
+}
+
+class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T angka1, T angka2, T angka3)
+    {
+        return (dynamic)angka1 + (dynamic)angka2 + (dynamic)angka3;
+    }
+}
